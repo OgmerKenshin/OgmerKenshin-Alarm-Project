1dcacec10d7c91b5e212c06170272b68b45c196e
OgmerKenshin <kenshinaogmer@gmail.com>
2024-09-29 05:07:37 +0800

    Youtube Mp3 converter Mini project.py

---
+++ basics recap side quest.py
@@ -0,0 +1,26 @@
+#this is not a project anymore because I'm too stupid for even beginner stuff
+print ("tanginang buhay to")
+#yan muna tas variables (string) nmn
+full_name = "Kentotan"
+
+print(f"pogi si {full_name}")
+
+#Now for integers (whole numbers)
+
+age = "18"
+
+print(f"hey, I'm {age} now, I can groom you")
+
+#pag floats naman pede magka decimal
+
+score = 69.420
+print(f"my score was {score}")
+
+# boolean naman is mga True or False statement
+is_student = True
+
+if is_student: 
+    print("you are a student")
+else: 
+    print("you are not a student")
+ 
